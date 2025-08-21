edfc0a626dde0f39b041082e99e4464dcec225ac
Anxhelika <krecianxhelika04@gmail.com>
2025-08-22 00:05:54 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const input = document.getElementById("tastinput");
+const btn = document.getElementById("shtodetyrebtn");
+const listadet = document.getElementById("detyrat");
+const filter = document.getElementById("filter");
+
+let listadetarr = [];
+
+// Ngarkimi i detyrave nga localStorage kur hap faqen
+const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+if (storedTasks) {
+    listadetarr = storedTasks;
+    shfaqlisten();
+}
+
+function shfaqlisten(filter = "all") {
+    listadet.innerHTML = "";
+
+    listadetarr.forEach(detyre => {
+        if(filter === "completed" && !detyre.completed) return;
+        if(filter === "pending" && detyre.completed) return;
+
+        const li = document.createElement("li");
+        li.classList.add("listel");
+        li.textContent = detyre.text;
+
+        if(detyre.completed) li.classList.add("completed");
+
+        const button = document.createElement("button");
+        button.classList.add("fshibtn");
+        button.textContent = "❌";
+
+        li.appendChild(button);
+        listadet.appendChild(li);
+
+        li.addEventListener("click", function() {
+            detyre.completed = !detyre.completed;
+            li.classList.toggle("completed");
+            localStorage.setItem("tasks", JSON.stringify(listadetarr));
+        });
+
+        button.addEventListener("click", function(e) {
+            e.stopPropagation();
+            listadetarr = listadetarr.filter(t => t.id !== detyre.id);
+            shfaqlisten(filter);
+            localStorage.setItem("tasks", JSON.stringify(listadetarr));
+        });
+    });
+}
+
+
+// Shtimi i detyrave
+btn.addEventListener('click', function() {
+    const text = input.value.trim();
+    if (text === "") return; 
+
+    let detyre = {
+        id: Date.now(),
+        text: text,
+        completed: false
+    };
+
+    listadetarr.push(detyre);
+    shfaqlisten();
+    localStorage.setItem("tasks", JSON.stringify(listadetarr));
+    input.value = "";
+});
+
+
+filter.addEventListener("change", function(){
+    const selectedfilter= filter.value;
+    shfaqlisten(selectedfilter);
+});
+
