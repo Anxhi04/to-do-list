4e156e901f685254f4057a8da720a7d13ddf5817
Anxhelika <krecianxhelika04@gmail.com>
2025-08-23 22:40:14 +0200

    shtimi i search bar dhe edit

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const input = document.getElementById("tastinput");
 const btn = document.getElementById("shtodetyrebtn");
 const listadet = document.getElementById("detyrat");
 const filter = document.getElementById("filter");
-
+const editmodal = document.getElementById("editmodal");
+const editcontent = document.getElementById("editContent");
+const cancelbutton = document.getElementById("cancelbtn");
+const donebutton = document.getElementById("donebtn");
+const search = document.getElementById("search");
 let listadetarr = [];
+let currentid = null;
 
 // Ngarkimi i detyrave nga localStorage kur hap faqen
 const storedTasks = JSON.parse(localStorage.getItem("tasks"));
@@ -12,16 +17,20 @@ if (storedTasks) {
     shfaqlisten();
 }
 
-function shfaqlisten(filter = "all") {
+function shfaqlisten(tasks = listadetarr, filter = "all") {
     listadet.innerHTML = "";
 
-    listadetarr.forEach(detyre => {
+    tasks.forEach(detyre => {
         if(filter === "completed" && !detyre.completed) return;
         if(filter === "pending" && detyre.completed) return;
 
         const li = document.createElement("li");
         li.classList.add("listel");
-        li.textContent = detyre.text;
+        const span = document.createElement("span");
+        span.textContent=detyre.text;
+        span.classList.add("note-text");
+        
+        li.appendChild(span);
 
         if(detyre.completed) li.classList.add("completed");
 
@@ -29,13 +38,23 @@ function shfaqlisten(filter = "all") {
         button.classList.add("fshibtn");
         button.textContent = "❌";
 
+        const editbtn = document.createElement("button");
+        editbtn.textContent="📝";
+
         li.appendChild(button);
+        li.appendChild(editbtn);
         listadet.appendChild(li);
 
         li.addEventListener("click", function() {
             detyre.completed = !detyre.completed;
             li.classList.toggle("completed");
             localStorage.setItem("tasks", JSON.stringify(listadetarr));
+        });
+        editbtn.addEventListener("click", function(e){
+            e.stopPropagation();
+            currentid = detyre.id;
+            editmodal.style.display="flex";
+            editcontent.value=li.querySelector(".note-text").textContent;
         });
 
         button.addEventListener("click", function(e) {
@@ -46,6 +65,20 @@ function shfaqlisten(filter = "all") {
         });
     });
 }
+
+        
+cancelbutton.addEventListener("click", function(){
+    editmodal.style.display="none";
+});
+
+donebutton.addEventListener("click", function(e){
+    e.stopPropagation();
+    const detyre = listadetarr.find(t=> t.id===currentid);
+    detyre.text=editcontent.value;
+    localStorage.setItem("tasks", JSON.stringify(listadetarr));
+    shfaqlisten();
+    editmodal.style.display="none";
+});
 
 
 // Shtimi i detyrave
@@ -68,6 +101,14 @@ btn.addEventListener('click', function() {
 
 filter.addEventListener("change", function(){
     const selectedfilter= filter.value;
-    shfaqlisten(selectedfilter);
+    shfaqlisten(listadetarr, selectedfilter);
 });
 
+//funx per kerkimin 
+search.addEventListener("input", function(){
+    const texti = search.value.toLowerCase();
+    const filteredTasks = listadetarr.filter(detyre =>
+    detyre.text.toLowerCase().includes(texti)
+);
+   shfaqlisten(filteredTasks);
+});
